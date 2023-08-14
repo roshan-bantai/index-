2c98a33c7ca7e17093de4945f3b2b82ada984986
roshan-bantai <programingroshan@gmail.com>
2023-08-14 16:49:00 +0530

    new changes

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 function onSignIn(googleUser) {
-  console.log("inside log in");
+  console.log("inside log in", googleUser);
   var profile = googleUser.getBasicProfile();
-  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //  console.log('Name: ' + profile.getName());
-  // console.log('Image URL: ' + profile.getImageUrl());
-  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   $("#name").text(profile.getName());
   $("#email").text(profile.getEmail());
   $("#image").attr("src", profile.getImageUrl());
